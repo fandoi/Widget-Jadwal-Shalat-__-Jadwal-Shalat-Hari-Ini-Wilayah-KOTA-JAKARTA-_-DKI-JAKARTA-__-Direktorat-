7113ba4532b584e498c76d0e86c16b799cfca484
fandoi <fandisasuke@gmail.com>
2017-06-01 01:50:31 +0000

    Add files via upload

---
+++ berbagi (2).php
@@ -0,0 +1,17 @@
+<?php
+$score = $_GET["score"];
+$scorex = explode(":", $score);
+$highscore = $scorex[0];
+?>
+<html>
+<head>
+<title>Kalender Puasa 2017</title>
+<meta property="og:title" content="Puasa Ramadhan <?echo $highscore;?> Hari lagi. Marhaban Yaa Ramadhan"/>
+<meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/katmis-126e2.appspot.com/o/sshare_0.png?alt=media&token=aa9e25fd-841e-46f8-9bc5-e0abfedabc62"/>
+<meta property="og:site_name" content="Kalender Puasa"/>
+<meta property="og:description" content="Download Aplikasi Kalender Puasa DISINI!"/>	
+</head>
+<body>
+<meta http-equiv="refresh" content="0;URL=https://play.google.com/store/apps/details?id=com.demidemu.KalenderPuasa" />
+</body>
+</html>
